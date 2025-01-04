3ffa563a9ff522bc21cd62445b5d179941bf35ad
Kevin Chen <kchen2561@gmail.com>
2025-01-03 21:37:14 -0600

    im dead

--- app/components/MainSection.js
+++ app/components/MainSection.js
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import styles from '../styles/MainSection.module.css';
+import styles from '@styles/MainSection.module.css';
 import { ReactTyped } from "react-typed";
 import { FaLinkedin, FaGithub, FaInstagram  } from "react-icons/fa";
 import { GiPreviousButton } from "react-icons/gi";
--- app/components/ProjectCard.js
+++ app/components/ProjectCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/ProjectCard.module.css'
+import styles from '@styles/ProjectCard.module.css';
 
 const ProjectCard = (props) => {
     // console.log(props.children)
